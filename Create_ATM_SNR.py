d8a050b5f767b7a4e5b56d1714d18780dd614946
Prajwal Niraula <prajwalniraula@dhcp-10-31-84-211.dyn.mit.edu>
2022-01-20 17:00:21 -0500

    Revised to name the important targets.

--- Create_ATM_SNR.py
+++ Create_ATM_SNR.py
@@ -9,6 +9,17 @@ import pandas as pd
 import mr_forecast as mr
 
 
+import matplotlib as mpl
+mpl.rc('font',**{'sans-serif':['Helvetica'], 'size':15,'weight':'bold'})
+mpl.rc('axes',**{'labelweight':'bold', 'linewidth':1.5})
+mpl.rc('ytick',**{'major.pad':22, 'color':'k'})
+mpl.rc('xtick',**{'major.pad':10,})
+mpl.rc('mathtext',**{'default':'regular','fontset':'cm','bf':'monospace:bold'})
+#mpl.rc('text', **{'usetex':True})
+#mpl.rc('text.latex',preamble=r'\usepackage{cmbright},\usepackage{relsize},'+r'\usepackage{upgreek}, \usepackage{amsmath}')
+mpl.rc('contour', **{'negative_linestyle':'solid'})
+
+
 MJup = 1.898e30 #mass of jupiters in grams
 RJup = 7.1492e9 #
 RadRatio = 10.973 #Radius of Jupiter to that of earth
@@ -78,11 +89,12 @@ for i in range(len(Name)):
     print("The current target is::", i)
     #Generate mass from the radius    
     if np.isfinite(PlanetaryMass[i]) and not(np.isfinite(PlanetaryRad[i])):
-        PlanetaryRad[i], _, _ = mr.Mstat2R(PlanetaryMass[i], 1e-9, sample_size=10, unit='Earth')
+        pass
+        #PlanetaryRad[i], _, _ = mr.Mstat2R(PlanetaryMass[i], 1e-9, sample_size=100, unit='Earth')
         #print("The planetary radius is :", PlanetaryRad[i], " and the mass is ", PlanetaryMass[i])  
     elif np.isfinite(PlanetaryRad[i]) and not(np.isfinite(PlanetaryMass[i])):
         #print("The value of given by ::", Name[i])
-        PlanetaryMass[i], _, _ = mr.Rstat2M(PlanetaryRad[i], 1e-9, sample_size=10, grid_size=1000,unit='Earth')  
+        PlanetaryMass[i], _, _ = mr.Rstat2M(PlanetaryRad[i], 1e-9, sample_size=1000, grid_size=1000,unit='Earth')  
         #print("The planetary mass is :", PlanetaryMass[i], " for the radius of the mass ", PlanetaryRad[i])
     else:
         print("Skipping this for now")
@@ -121,42 +133,71 @@ PlanetaryRad = np.ascontiguousarray(PlanetaryRad[~RemoveIndex])
 Grav_pl = Grav_pl[~RemoveIndex]
 
 SelectIndex = np.where(Name=="TRAPPIST-1 b")
+SNR = SNR/SNR[SelectIndex]
 
-SelectIndex1 = np.where(Name=="K2-315 b")
+#Arrange by the SNR
+#ArrangeIndex = np.argsort(SNR)[::-1]
 
 
-SNR = np.log10(S/S[SelectIndex])
-
-
+#Name = np.ascontiguousarray(Name[ArrangeIndex])
+#T_Eff_Planet = np.ascontiguousarray(T_Eff_Planet[ArrangeIndex])
+#S = np.ascontiguousarray(S[ArrangeIndex])
+#SNR = np.ascontiguousarray(SNR[ArrangeIndex])
+#PlanetaryRad = np.ascontiguousarray(PlanetaryRad[ArrangeIndex])
+#Grav_pl = Grav_pl[ArrangeIndex]
 
 
 #Now the get the size of the signal for JWST
 print("The gravity of the planet is given by:: ", Grav_pl[SelectIndex])
 
 
+print("The first case:", Name[0])
+print("The second case:", Name[1])
+
+
+
+
+
+
+
+
 
 #The color bar is given by SNR
 plt.figure(figsize=(12,8))
 
-plt.scatter(T_Eff_Planet[0], S[0], s=(PlanetaryRad[0]*10000)**0.5, edgecolor='navy', lw=2, zorder=20)
-plt.scatter(T_Eff_Planet[1], S[1], s=(PlanetaryRad[1]*10000)**0.5, edgecolor='navy', lw=2, zorder=20)
-
-plt.text(T_Eff_Planet[0]+30, S[0], "TOI-4306 b", va="center", color="navy" )
-plt.text(T_Eff_Planet[1]+30, S[1], "SPECULOOS-2 b", va="center", color="navy")
+plt.scatter(T_Eff_Planet[0], S[0], s=(PlanetaryRad[0]*100), c="None", edgecolor='navy', lw=2, zorder=20)
+plt.scatter(T_Eff_Planet[1], S[1], s=(PlanetaryRad[1]*100), c="None", edgecolor='navy', lw=2, zorder=20)
 
 
+plt.scatter(T_Eff_Planet[SelectIndex], S[SelectIndex], s=(PlanetaryRad[SelectIndex]*10000)**0.5, c="None", edgecolor='black', lw=2, zorder=20)
+#plt.text(T_Eff_Planet[SelectIndex]+30, S[SelectIndex], "TRAPPIST-1 b", va="center", color="black")
 
-plt.scatter(T_Eff_Planet[SelectIndex], S[SelectIndex], s=(PlanetaryRad[SelectIndex]*10000)**0.5, edgecolor='red', lw=2, zorder=20)
-plt.text(T_Eff_Planet[SelectIndex]+30, S[SelectIndex], "TRAPPIST-1 b", va="center", color="red")
+#for 1.5 vs 1.0 Earth radii
+plt.scatter(975, 230, s=(1.5*100), c="k", edgecolor='None', lw=2, zorder=20)
+plt.text(1000,  230, "1.5 $R_{\oplus}$", va="center")
 
+plt.scatter(975, 220, s=(100), c="k",  edgecolor='None', lw=2, zorder=20)
+plt.text(1000,  220, "1.0 $R_{\oplus}$", va="center")
 
-Scatter = plt.scatter(T_Eff_Planet, S, c=SNR, s=(PlanetaryRad*10000)**0.5, edgecolor='None', cmap='brg')
+RightHandSide = np.array(["SPECULOOS-2 b", "TRAPPIST-1 f", "TRAPPIST-1 e", "TRAPPIST-1 h", "K2-315 b",  "K2-72 b", "LHS 1140 c"])
+
+for i in range(len(Name)):
+    if S[i]>75.0 and T_Eff_Planet[i]<1000:
+        if not(np.sum(Name[i]==RightHandSide)==1):
+            plt.text(T_Eff_Planet[i]+20, S[i], Name[i], va="center")
+        else:
+            plt.text(T_Eff_Planet[i]-20, S[i], Name[i], ha="right", va="center")
+
+    print(i, ":", Name[i])
+
+Scatter = plt.scatter(T_Eff_Planet, S, c=SNR, s=(PlanetaryRad*100), edgecolor='None', cmap='rainbow', vmax=1.05, vmin=0.001)
 cbar = plt.colorbar()
 cbar.ax.get_yaxis().labelpad = 20
 cbar.ax.set_ylabel("Relative SNR", fontsize=20, rotation=270)
 plt.ylabel("Expected Transmission Signal (ppm)", fontsize=20)
 plt.xlabel("Equilibrium Temperature (K)", fontsize=20)
 plt.ylim(5, 250)
+plt.xlim(10, 1100)
 #plt.yscale('log')
 plt.yticks([12.5, 25., 50., 100., 200.,])
 plt.tight_layout()
